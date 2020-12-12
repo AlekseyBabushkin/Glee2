122d903ba58034b44687f87dec953d31374819d9
AlekseyBabushkin <babushkin058@mail.ru>
2020-12-12 19:52:20 +0200

    added section product

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,17 @@
 $(function(){
 
- $('.slider__wrapper').slick({
+ $('.slider__top').slick({
    dots: true,
    arrows:false,
  });
+
+ $('.button-nav__item').click(function(){
+   $('.button-nav__item').toggleClass('button-nav__item--active')
+ });
+  
+
+//  $('.products').on('click', function(){
+//    $('.button-nav__item').toggleClass('button-nav__item--active')
+//  })
 
 });
