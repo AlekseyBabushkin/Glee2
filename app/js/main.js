221a4e5eea3c3a79847cfe972af6e8d3d55f4cd2
AlekseyBabushkin <babushkin058@mail.ru>
2021-01-06 23:17:25 +0200

    added detalis

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,29 @@
 $(function () {
+  
+  $('.detalis-content__small').slick({
+    asNavFor: '.detalis-content__big',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    vertical: true,
+    draggable: false,
+  });
+
+  $('.detalis-content__big').slick({
+    asNavFor: '.detalis-content__small',
+    draggable: false,
+    arrows: false,
+    fade: true,
+  });
+
+  $('.detalis-related__slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+  })
+
+  $('.detalis-content__input').styler();
 
   $('.filter-price__input').ionRangeSlider({
     type: "double",
